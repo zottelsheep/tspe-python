40fd2f108ad73cf28cfb9ea140363b93aaede912
Felician Richter <s190655@th-ab.de>
2023-03-20 16:24:18 +0100

    Feat: Add spiketrain loaders

---
+++ tspe/util.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+from typing import Optional, Union
+
+from scipy.io import loadmat
+from quantities import millisecond, Quantity
+from elephant.conversion import BinnedSpikeTrain
+from neo import SpikeTrain
+
+DEFAULT_BIN_SIZE = 10 * millisecond
+
+def load_spike_train_example_mat(path: Union[Path,str],bin_size: Optional[Quantity] = None):
+
+    if isinstance(path,str):
+        path = Path(path)
+
+    if not bin_size:
+        bin_size = DEFAULT_BIN_SIZE
+
+
+    data = loadmat(path,simplify_cells=True)['data']
+
+    if 'asdf' not in data:
+        raise ValueError('Incorrect Dataformat: Missing spiketrain_data in "asdf"')
+
+    spiketrain_data = data['asdf']
+
+    # Get number of electrodesa and recording_duration from last element of data array
+    n_electrodes, recording_duration_ms = spiketrain_data[-1]
+
+    # Create spiketrains
+    spiketrains = []
+    for spiketrain_raw in spiketrain_data[0:n_electrodes]:
+        spiketrains.append(SpikeTrain(spiketrain_raw*millisecond, t_stop=recording_duration_ms*millisecond))
+
+    spiketrains = BinnedSpikeTrain(spiketrains,bin_size=bin_size)
+
+    return spiketrains
+
+def load_spike_train_mat(path: Union[Path,str],bin_size: Optional[Quantity] = None):
+
+    if isinstance(path,str):
+        path = Path(path)
+
+    if not bin_size:
+        bin_size = DEFAULT_BIN_SIZE
+
+
+    data = loadmat(path,simplify_cells=True)['SPIKEZ']
+
+    if 'TS' not in data:
+        raise ValueError('Incorrect dataformat: No timestamps in data under "TS"')
+
+    spiketrain_timestamps = data['TS']
+
+    if 'PREF' not in data:
+        raise ValueError('Incorrect dataformat: No metadata in data under "PREF"')
+
+    spiketrain_metadata = data['PREF']
+
+    # Get number of electrodesa and recording_duration from last element of data array
+    n_electrodes = len(spiketrain_timestamps)
+    recording_duration_ms = spiketrain_metadata['rec_dur'] * millisecond
+
+    # Create spiketrains
+    spiketrains = []
+    for spiketrain_raw in spiketrain_timestamps[0:n_electrodes]:
+        spiketrains.append(SpikeTrain(spiketrain_raw*millisecond, t_stop=recording_duration_ms))
+
+    spiketrains = BinnedSpikeTrain(spiketrains,bin_size=bin_size)
+
+    return spiketrains
+
