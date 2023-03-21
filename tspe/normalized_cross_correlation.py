612574224b25a47cdf1c542a1cd5e3422014b1f9
Felician Richter <s190655@th-ab.de>
2023-03-21 17:05:42 +0100

    Feat: Compute multiple delays in normalized_cross_correlation

--- tspe/normalized_cross_correlation.py
+++ tspe/normalized_cross_correlation.py
@@ -1,8 +1,12 @@
+from typing import List, Iterable, Union
+
 from elephant.conversion import BinnedSpikeTrain
 import numpy as np
 
 
-def normalized_cross_correlation(spike_trains: BinnedSpikeTrain, delay_time:int = 0) -> np.ndarray:
+def normalized_cross_correlation(
+    spike_trains: BinnedSpikeTrain, delay_times: Union[int, List[int], Iterable[int]] = 0
+) -> np.ndarray:
     r"""normalized cross correlation using std deviation
 
     Computes the normalized_cross_correlation between all
@@ -11,35 +15,50 @@ def normalized_cross_correlation(spike_trains: BinnedSpikeTrain, delay_time:int 
     The underlying formula is:
 
     .. math::
-        NCC_{X\arrY(d)} = \frac{1}{N_{bins}}\sum_{i=-\inf}^{\inf}{\frac{(y_{(i)} - \bar{y}) \cdot (x_{(i-d) - \bar{x})}{\sigma_x \cdot \sigma_y}}
+        NCC_{X\arrY(d)} = \frac{1}{N_{bins}}\sum_{i=-\inf}^{\inf}{\frac{(y_{(i)} - \bar{y}) \cdot (x_{(i-d) - \bar{x})}{\sigma_x \cdot \sigma_y}}}
 
     """
 
-    n_bins = spike_trains.n_bins
+    n_neurons, n_bins = spike_trains.shape
 
     # Get sparse array of BinnedSpikeTrain
     # TODO: Check effekts of binary spike-train (spike_trains.binarize())
     spike_trains_array = spike_trains.to_sparse_array()
 
     # Get mean of spike_train
-    ones = np.ones((n_bins,1))
-    spike_trains_mean = ( spike_trains_array * ones ) / n_bins
+    ones = np.ones((n_bins, 1))
+    spike_trains_mean = (spike_trains_array * ones) / n_bins
     spike_trains_zeroed = spike_trains_array - spike_trains_mean
 
     # Get std deviation of spike trains
-    spike_trains_std = np.std(spike_trains_zeroed,axis=1)
+    spike_trains_std = np.std(spike_trains_zeroed, axis=1)
 
-    # Compute cross correlation at given delay time
-    # Uses theoretical zero-padding for shifted values,
-    # but since $0 \cdot x = 0$ values can simply be omitted
-    if delay_time >= 0:
-        CC = spike_trains_zeroed[:,delay_time:] * np.transpose(spike_trains_zeroed[:,:-delay_time or None])
-    else:
-        CC = spike_trains_zeroed[:,:delay_time] * np.transpose(spike_trains_zeroed[:,-delay_time or None:])
+    # Loop over delay times
+    if isinstance(delay_times, int):
+        delay_times = [delay_times]
+    elif isinstance(delay_times, Iterable):
+        delay_times = list(delay_times)
 
-    # Normalize using std deviation
-    std_factors = spike_trains_std * np.transpose(spike_trains_std)
-    NCC = CC / std_factors / n_bins
+    NCC_d = np.zeros((len(delay_times), n_neurons, n_neurons))
 
-    return NCC
+    for delay_time in delay_times:
+        # Uses theoretical zero-padding for shifted values,
+        # but since $0 \cdot x = 0$ values can simply be omitted
+        if delay_time >= 0:
+            CC = spike_trains_array[:, delay_time:] * np.transpose(
+                spike_trains_array[:, : -delay_time or None]
+            )
+        else:
+            CC = spike_trains_array[:, :delay_time] * np.transpose(
+                spike_trains_array[:, -delay_time or None :]
+            )
 
+        # Normalize using std deviation
+        std_factors = spike_trains_std * np.transpose(spike_trains_std)
+        NCC = CC / std_factors / n_bins
+
+        # Compute cross correlation at given delay time
+        NCC_d[delay_time, :, :] = NCC
+
+
+    return NCC_d
--- tspe/tspe.py
+++ tspe/tspe.py
@@ -27,24 +27,20 @@ def total_spiking_probability_edges(
     filter_pairs = generate_filter_pairs(a, b, c)
     n_filter_pairs = len(filter_pairs)
 
-    max_length_edge_filter = 2 * max(a) + max(b) + 2 * max(c)
+    max_delay = 2 * max(a) + max(b) + 2 * max(c) + 25
 
     # Calculate normalized cross corelation for different delays
-    NCC_d = np.zeros((max_length_edge_filter + 25, n_neurons, n_neurons))
-    for delay_time in range(max_length_edge_filter + 25):
-        NCC_d[delay_time, :, :] = normalized_cross_correlation(
-            spike_train_data, delay_time
-        )
+    NCC_d = normalized_cross_correlation(spike_train_data, delay_times=range(max_delay))
 
 
     # Apply edge and running total filter
     neuron_pairs = np.tril_indices(n_neurons)
     n_neurons_pairs = len(neuron_pairs[0])
-    TSPE = np.zeros((n_neurons_pairs, max_length_edge_filter + 25))
+    TSPE = np.zeros((n_neurons_pairs, max_delay))
     for edge_filter, running_total_filter in filter_pairs:
         for n_neuron_pair, (neuron_1, neuron_2) in enumerate(zip(*neuron_pairs)):
-            x1 = np.convolve(NCC_d[:, neuron_1, neuron_2], edge_filter, 'valid')
-            x2 = np.convolve( x1, running_total_filter, 'full')
+            x1 = np.convolve(NCC_d[:, neuron_1, neuron_2], edge_filter, "valid")
+            x2 = np.convolve(x1, running_total_filter, "full")
 
             TSPE[n_neuron_pair, :len(x2)] += x2
 
