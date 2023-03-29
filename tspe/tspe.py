3b40d619e27c981ce1ec7fac3dab77efb56e8606
Felician Richter <s190655@th-ab.de>
2023-03-30 01:36:41 +0200

    Feat: Add utility-wrapper for tspe

--- tspe/tspe.py
+++ tspe/tspe.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 from elephant.conversion import BinnedSpikeTrain
 import numpy as np
@@ -8,14 +8,19 @@ from tspe.filters import generate_filter_pairs
 from tspe.normalized_cross_correlation import normalized_cross_correlation
 
 def tspe(
-    spike_train_data: BinnedSpikeTrain,
+    spike_trains: BinnedSpikeTrain,
     a: Optional[List[int]] = None,
     b: Optional[List[int]] = None,
     c: Optional[List[int]] = None,
     max_delay: int = 25
          ):
 
-    delay_matrix = total_spiking_probability_edges(spike_train_data,a,b,c,max_delay)
+    tspe_matrix = total_spiking_probability_edges(spike_trains,a,b,c,max_delay)
+
+    connectivity_matrix, delay_matrix = get_connectivity_matrix(tspe_matrix)
+
+    return connectivity_matrix, delay_matrix
+
 
 
 def total_spiking_probability_edges(
@@ -46,7 +51,7 @@ def total_spiking_probability_edges(
     NCC_d = normalized_cross_correlation(spike_trains, delay_times=delay_range)
 
     # Apply edge and running total filter
-    delay_matrix = np.zeros((n_neurons, n_neurons, max_delay))
+    tspe_matrix = np.zeros((n_neurons, n_neurons, max_delay))
     for filter in filter_pairs:
         # Select ncc_window based on needed filter padding
         NCC_window = NCC_d[:,:,max_padding-filter.needed_padding:max_delay+max_padding+filter.needed_padding]
@@ -55,16 +60,17 @@ def total_spiking_probability_edges(
         x1 = fftconvolve(NCC_window, np.expand_dims(filter.edge_filter,(0,1)), mode="valid",axes=2)
         x2 = fftconvolve(x1, np.expand_dims(filter.running_total_filter,(0,1)), mode="full",axes=2)
 
-        delay_matrix += x2
+        tspe_matrix += x2
 
-    return delay_matrix
+    return tspe_matrix
 
-def generate_connectivity_matrix_from_delay_matrix(delay_matrix: np.ndarray) -> np.ndarray:
+def get_connectivity_matrix(tspe_matrix: np.ndarray) -> Tuple[np.ndarray,np.ndarray]:
 
     # Take maxima of absolute of delays to get estimation for connectivity
-    connectivity_matrix_index = np.argmax(np.abs(delay_matrix),axis=2,keepdims=True)
-    connectivity_matrix = np.take_along_axis(delay_matrix,connectivity_matrix_index,axis=2).squeeze(axis=2)
+    connectivity_matrix_index = np.argmax(np.abs(tspe_matrix),axis=2,keepdims=True)
+    connectivity_matrix = np.take_along_axis(tspe_matrix,connectivity_matrix_index,axis=2).squeeze(axis=2)
+    delay_matrix = connectivity_matrix_index
 
-    return connectivity_matrix
+    return connectivity_matrix, delay_matrix
 
 
