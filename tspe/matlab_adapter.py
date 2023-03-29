4082c45a17705046d4d905f14c66e78f76c213bf
Felician Richter <s190655@th-ab.de>
2023-03-30 01:36:24 +0200

    Feat: Handle different arguments to tspe in matlab

--- tspe/matlab_adapter.py
+++ tspe/matlab_adapter.py
@@ -1,18 +1,20 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 from pathlib import Path
 
 import matlab.engine
 from elephant.conversion import BinnedSpikeTrain
 import numpy as np
+import numpy.typing as npt
 
 from tspe.io import convert_to_sdf
 
 
 def total_spiking_probability_edges_matlab(
-    a: Optional[List[int]] = None,
-    b: Optional[List[int]] = None,
-    c: Optional[List[int]] = None,
     spike_trains: BinnedSpikeTrain,
+    a: Optional[npt.ArrayLike] = None,
+    b: Optional[npt.ArrayLike] = None,
+    c: Optional[npt.ArrayLike] = None,
+    max_delay: int = 25,
     matlab_engine: Optional[matlab.engine.MatlabEngine] = None,
     tspe_path: Optional[Path] = None,
 ):
@@ -31,17 +33,43 @@ def total_spiking_probability_edges_matlab(
         own_matlab_engine = False
 
     # Change path to tspe_path
+    old_path = matlab_engine.pwd()
     matlab_engine.cd(str(tspe_path.absolute()))
+
+    # Convert options
+    if a is not None:
+        a = np.array(a,dtype=np.float64)
+    else:
+        a = np.array([])
+
+
+    if b is not None:
+        b = np.array(b,dtype=np.float64)
+    else:
+        b = np.array([])
+
+    if c is not None:
+        c = np.array(c,dtype=np.float64)
+    else:
+        c = np.array([])
+
+    if max_delay is not None:
+        d = np.float64(max_delay)
+    else:
+        d = np.array([])
 
     # Convert spike_train_data into SpikeDataFormat
     sdf = convert_to_sdf(spike_trains)
 
     # Call TSPE-Function via matlab
-    connectivity_matrix, delay_matrix = matlab_engine.TSPE(sdf,nargout=2)
+    connectivity_matrix, delay_matrix = matlab_engine.TSPE(sdf,d,a,b,c,nargout=2)
 
     # Handle return types
     connectivity_matrix = np.array(connectivity_matrix)
     delay_matrix = np.array(delay_matrix)
+
+    # Restore old path
+    matlab_engine.cd(old_path)
 
     # Quit matlab if own session was started
     if own_matlab_engine:
